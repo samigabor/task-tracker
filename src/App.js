1699fbb5758dce2997fa32e75195fbca37046b77
s-gabor <ing.gabor@yahoo.com>
2022-01-28 17:04:49 +0200

    add task, tasks components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+
+const MOCK_DATA = [1, 2, 3].map((id) => ({
+  name: `Task ${id}`,
+  day: `Day ${id}`,
+  id,
+}));
 
 function App() {
+  const [tasks, setTasks] = useState(MOCK_DATA);
+
+  const onDelete = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
   return (
     <div className="container">
       <Header title={"Task Tracker"} />
+      <Tasks tasks={tasks} onDelete={onDelete} />
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,9 @@
+import Task from "./Task";
+
+const Tasks = ({ tasks, onDelete }) => {
+  return tasks.map((task) => (
+    <Task task={task} onDelete={onDelete} key={task.id} />
+  ));
+};
+
+export default Tasks;
